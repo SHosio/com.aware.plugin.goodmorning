6f15dd4497bafb8116b0fb99238a0e883a544891
Simo Hosio <simo@simohosio.com>
2015-02-10 20:52:30 +0200

    Initial commit

---
+++ app/src/main/java/com/aware/plugin/goodmorning/Plugin.java
@@ -0,0 +1,27 @@
+package com.aware.plugin.goodmorning;
+
+import android.util.Log;
+
+import com.aware.Aware;
+import com.aware.Aware_Preferences;
+import com.aware.utils.Aware_Plugin;
+
+public class Plugin extends Aware_Plugin {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        TAG = "Template";
+        DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
+
+        if( DEBUG ) Log.d(TAG, "Template plugin running");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if( DEBUG ) Log.d(TAG, "Template plugin terminated");
+    }
+}
